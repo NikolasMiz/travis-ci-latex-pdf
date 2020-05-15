da59bc946ef1873f21c1bb9a49ed1e5540db821e
Nicholas <miazzo.nicholas98@gmail.com>
2020-05-15 13:56:01 +0200

    Update texlive_install.sh

--- 4-texlive/texlive/texlive_install.sh
+++ 4-texlive/texlive/texlive_install.sh
@@ -10,8 +10,8 @@
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
-  # wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-  wget http://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
+   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+ # wget http://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
 
